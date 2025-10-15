9c16533c82ebd96834f73e696a6cdfb10bf79c64
xiaoputao5678 <xiaoputao5678@outlook.com>
2025-10-15 16:41:10 +0800

    Delete superset directory

--- superset/1初始化.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001
-set SUPERSET_HOME=%cd%\db
-del %SUPERSET_HOME%\examples.db
-del %SUPERSET_HOME%\superset.db
-.\python.exe -m pip uninstall apache_superset -y
-echo "初始化完成,可以关闭窗口了！！！"
-timeout /t 30 /nobreak
--- superset/3启动.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001                                
-set SUPERSET_HOME=%cd%\db
-echo "数据库存放路径："%SUPERSET_HOME%
-set SUPERSET_SECRET_KEY="dFY9WJhbrsZzOGP4QKqa2M4Ct7zvuzI6"
-set FLASK_APP=superset
-.\Scripts\superset run -h 0.0.0.0 -p 8088 --with-threads --reload
